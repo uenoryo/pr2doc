c3248f9242d327065f71a948b62ec5055898311a
uenoryo <r.r.ag0611@outlook.jp>
2018-09-08 11:35:46 +0900

    add findPRNumber test.

--- pr2doc_test.go
+++ pr2doc_test.go
@@ -42,3 +42,52 @@ func Test_findDescription(t *testing.T) {
         })
     }
 }
+
+func Test_findPRNumber(t *testing.T) {
+    type test struct {
+        Title  string
+        Input  string
+        Output int
+    }
+
+    tests := []test{
+        {
+            Title:  "success",
+            Input:  "Merge pull request #1234 from pr2doc/develop",
+            Output: 1234,
+        },
+        {
+            Title:  "success (too many number)",
+            Input:  "Merge pull request #123 #567 #789 from pr2doc/develop",
+            Output: 123,
+        },
+        {
+            Title:  "error (missing PR number requires # )",
+            Input:  "Merge pull request 12345 from pr2doc/develop",
+            Output: 0,
+        },
+        {
+            Title:  "error (no spacing)",
+            Input:  "Merge pull request#987 from pr2doc/develop",
+            Output: 0,
+        },
+        {
+            Title:  "error (too large number)",
+            Input:  "Merge pull request #456789 from pr2doc/develop",
+            Output: 0,
+        },
+    }
+
+    for _, test := range tests {
+        t.Run(test.Title, func(t *testing.T) {
+            p2d := &Pr2Doc{}
+            prNum, err := p2d.findPRNumber(test.Input)
+            if err != nil {
+                t.Fatal("error findPRNumber", err.Error())
+            }
+            if g, w := prNum, test.Output; g != w {
+                t.Errorf("error find PR number %d, want %d", g, w)
+            }
+        })
+    }
+}
