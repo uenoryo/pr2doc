b392154a99bcfe90da1d5a35b5f5eb05cf7b2dcf
uenoryo <r.r.ag0611@outlook.jp>
2018-09-08 13:28:01 +0900

    add example.

---
+++ example/example.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/uenoryo/pr2doc"
+)
+
+const (
+	repoOwner = "owner"
+	repoName  = "repo-name"
+	token     = "github-access-token"
+)
+
+func main() {
+	ctx := context.Background()
+	gs := pr2doc.NewGithubService(ctx, repoOwner, repoName, token)
+	p2d := pr2doc.NewPr2Doc(gs)
+	doc, err := p2d.Run(context.Background(), os.Args[1])
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(doc)
+}
